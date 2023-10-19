b8da7cf82bd6316ea910142b443d5d0d9be7f2c7
tituskinu <mwaura.tk@gmail.com>
2023-10-18 19:01:43 -0700

    employee, login, constants, config, runnerClass, feature

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,52 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utils.CommonMethods;
+import utils.ConfigReader;
+
+import java.time.Duration;
+
+public class LoginSteps extends CommonMethods {
+    // public WebDriver driver;
+
+    @Given("user is navigated to HRMS application")
+    public void user_is_navigated_to_hrms_application() throws InterruptedException {
+       // driver = new ChromeDriver();
+       // driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
+        // Thread.sleep(2000);
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        openBrowserAndLaunchApplication();
+    }
+
+    @When("user enters admin username and password")
+    public void user_enters_admin_username_and_password() throws InterruptedException {
+        WebElement usernameTextField = driver.findElement(By.xpath("//*[@id='txtUsername']"));
+       // usernameTextField.sendKeys("admin");
+        //  Thread.sleep(2000);
+        sendText(usernameTextField, ConfigReader.getPropertyValue("username"));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebElement passwordTextField = driver.findElement(By.cssSelector("input#txtPassword"));
+        //passwordTextField.sendKeys("Hum@nhrm123");
+        sendText(passwordTextField, ConfigReader.getPropertyValue("password"));
+    }
+
+    @When("user clicks on login button")
+    public void user_clicks_on_login_button() throws InterruptedException {
+        WebElement loginButton = driver.findElement(By.xpath("//*[@value='LOGIN']"));
+        click(loginButton);
+       // loginButton.click();
+        //    Thread.sleep(2000);
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Then("user is successfully logged in")
+    public void user_is_successfully_logged_in() {
+        System.out.println("My test passed");
+    }
+}
